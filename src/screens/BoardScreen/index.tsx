0010a2370d4c11f77240763f54d84708c88e17a5
chogyejin <whrpwls96@naver.com>
2021-05-03 18:02:38 +0900

    게시판 탭할 시 post 불러오기

--- src/screens/BoardScreen/index.tsx
+++ src/screens/BoardScreen/index.tsx
@@ -27,24 +27,58 @@ interface BoardScreenProps {
   navigation: BoardScreenNavigationProps;
 }
 
+interface Post {
+  id: string;
+  userID: string;
+  schoolID: string;
+  title: string;
+}
 const BoardScreen: React.FunctionComponent<BoardScreenProps> = (props) => {
   const { navigation } = props;
+  const [userID, setUserID] = useState('');
+  const [posts, setPosts] = useState<Post[]>([]);
+  // posts 타입 정의해주기
+  const movePost = (postID: string) => () => {
+    // 게시글 상세 페이지로 이동한다.
+    // 이동할 때 postID를 같이 넘겨서 이동한다.
+
+    console.log('asd');
+  };
+
+  // 이 아래있는 주석은 다른 파일에서 이뤄지는 행동(게시글 상세 페이지가 새로 생겨야 한다.)
+  // 게시글 상세 페이지에서는 받은 postID를 통해서
+  // useEfeect 코드를 통해 그 post.id로 api요청을 하고
+  // 요청해서 받ㅇ느 데이터를 state저장해서 보여주면 된다.
 
   useEffect(() => {
-    console.log('렌덜링이 완료');
-  });
+    async function getPost() {
+      const result = await axios.get('http://localhost:4000/api/posts', {
+        params: {
+          schoolID: 'S010000391',
+        },
+      });
 
-  console.log('렌더링');
+      if (result.data) {
+        if (posts.length === 0) {
+          setPosts(result.data.posts);
+        }
+        console.log(posts[0]);
+      } else {
+        console.log('실패');
+      }
+    }
+    getPost();
+  }, [posts]);
 
   return (
     <SafeAreaView>
-      <Text style={styles.ScreenTitle}>board 테스트</Text>
+      <Text style={styles.ScreenTitle}>학교 게시판 테스트</Text>
       <View style={styles.container}>
-        <View>
-          <TouchableOpacity>
-            <Text style={styles.BoardTitle}>게시판1</Text>
-          </TouchableOpacity>
-        </View>
+        {posts.map((post) => (
+          <Text onPress={movePost(post.id)} key={post.id}>
+            {post.title}
+          </Text>
+        ))}
       </View>
     </SafeAreaView>
   );

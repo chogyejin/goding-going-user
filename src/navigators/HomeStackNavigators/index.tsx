1be76e1f0b48c33d197bea2d0a330d06e9efdec9
yatyat2 <xowls2287@naver.com>
2021-05-08 15:51:31 +0900

    에러 코드 삭제

--- src/navigators/HomeStackNavigators/index.tsx
+++ src/navigators/HomeStackNavigators/index.tsx
@@ -8,10 +8,6 @@ import BoardScreen, { BoardParams } from '../../screens/BoardScreen';
 import BoardDetail, {
   BoardDetailParams,
 } from '../../screens/BoardScreen/BoardDetail';
-import EtcScreen, { EtcParams } from '../../screens/EtcScreen';
-import TeacherTip, {
-  TeacherTipParams,
-} from '../../screens/EtcScreen/TeacherTip';
 
 // 1. 필요한 스크린에 대해 enum 타입을 정의한다. (리듀서에서 액션타입을 지정해주는 것 처럼)
 export enum HomeScreens {
@@ -31,8 +27,6 @@ export type HomeStackParamList = {
   SignUp: SignUpParams;
   Board: BoardParams;
   BoardDetail: BoardDetailParams;
-  Etc: EtcParams;
-  TeacherTip: TeacherTipParams;
 };
 
 // 3. 방금 만든 타입을 createStackNavigator 메소드 앞에 지정해주서 HomeStack 네비게이터 객체를 만들어줌.
@@ -48,8 +42,6 @@ const HomeStackNavigator: React.FunctionComponent = () => {
         name={HomeScreens.BoardDetail}
         component={BoardDetail}
       />
-      <HomeStack.Screen name={HomeScreens.Etc} component={EtcScreen} />
-      <HomeStack.Screen name={HomeScreens.TeacherTip} component={TeacherTip} />
     </HomeStack.Navigator>
   );
 };

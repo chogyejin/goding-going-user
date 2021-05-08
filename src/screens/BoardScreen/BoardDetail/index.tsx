78f053b29c55037de5f25f731ab8149ca234aef6
chogyejin <whrpwls96@naver.com>
2021-05-08 11:26:31 +0900

    BoardDetail Post 인터페이스 작성, useEffect에 api 요청 코드 작성

--- src/screens/BoardScreen/BoardDetail/index.tsx
+++ src/screens/BoardScreen/BoardDetail/index.tsx
@@ -10,8 +10,10 @@ import {
   View,
   TouchableOpacity,
   Dimensions,
+  ShadowPropTypesIOS,
 } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import axios from 'axios';
 
 type BoardDetailNavigationProps = StackNavigationProp<
   HomeStackParamList,
@@ -27,6 +29,10 @@ interface BoardDetailProps {
   navigation: BoardDetailNavigationProps;
 }
 
+interface Post {
+  post: string;
+}
+
 // 게시글 상세 페이지에서는 받은 postID를 통해서
 // useEfeect 코드를 통해 그 post.id로 api요청을 하고
 // 요청해서 받ㅇ느 데이터를 state저장해서 보여주면 된다.
@@ -34,15 +40,31 @@ const BoardDetail: React.FunctionComponent<BoardDetailProps> = (props) => {
   const { navigation, route } = props;
   const { params } = route;
   const { postID } = params;
+  const [post, setPost] = useState<Post>();
 
   useEffect(() => {
-    console.log('useEffect');
+    async function showPost() {
+      const result = await axios.get('http://localhost:4000/api/posts', {
+        params: {
+          postID: postID,
+        },
+      });
+
+      if (result) {
+        console.log('성공');
+        setPost(post);
+      } else {
+        console.log('실패');
+      }
+    }
+    showPost();
   });
 
   return (
     <SafeAreaView>
       <Text style={styles.BoardDetailTitle}>게시판 상세</Text>
       <Text style={styles.BoardDetailTitle}>{postID}</Text>
+      <Text style={styles.BoardDetailTitle}>{post}</Text>
     </SafeAreaView>
   );
 };
